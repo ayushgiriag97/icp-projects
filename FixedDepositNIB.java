1bc69cc0084d989f4e79ed81ba53bebf476126a6
ayushgiriag97 <ayushgiri.ag.97@gamil.com>
2025-12-19 17:05:23 +0545

    Add FixedDepositNIB.java

---
+++ FixedDepositNIB.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FixedDepositNIB{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name =");
+        System.out.println("\n");
+        String nameOfAccHolder=sc.next();
+        System.out.println("Amount =");
+        int principle=sc.nextInt();
+        System.out.println("\n");
+        System.out.println("Duration =");
+        int time=sc.nextInt();
+        System.out.println("\n");
+        
+        
+    }
+}
