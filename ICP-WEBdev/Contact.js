0a16157a5e2d4a05d436a38f046154478b014bbb
ayushgiriag97 <ayushgiri.ag.97@gamil.com>
2026-07-09 14:09:43 +0545

    refactor: rename ICP-WEBdev to Ecomart-TeamProject and add README.md

--- ICP-WEBdev/Contact.js
+++
@@ -1,7 +0,0 @@
-
- // Form submission handler
-document.querySelector('form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Thank you for contacting us! We will get back to you soon.');
-            this.reset();
-        });
